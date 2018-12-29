ae2397cc307239934b8b1fc8f84c8fe42f94fb2b
Navv <grzesiek.kab@gmail.com>
2018-12-29 19:25:29 +0100

    added xy co-ordinates to board

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,96 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 
 public class Controller {
 
+    Field blueSpawn1 = new Field(4,329,true);
+    Field blueSpawn2 = new Field(40,329,true);
+    Field blueSpawn3 = new Field(4,365,true);
+    Field blueSpawn4 = new Field(40,365,true);
+
+    Field blueFinish1 = new Field(185,330,false);
+    Field blueFinish2 = new Field(185,293,false);
+    Field blueFinish3 = new Field(185,258,false);
+    Field blueFinish4 = new Field(185,221,false);
+
+    Field yellowSpawn1 = new Field(4,3,true);
+    Field yellowSpawn2 = new Field(40,3,true);
+    Field yellowSpawn3 = new Field(4,39,true);
+    Field yellowSpawn4 = new Field(39,39,true);
+
+    Field yellowFinish1 = new Field(40,184,false);
+    Field yellowFinish2 = new Field(76,184,false);
+    Field yellowFinish3 = new Field(112,184,false);
+    Field yellowFinish4 = new Field(149,184,false);
+
+    Field greenSpawn1 = new Field(331,3,true);
+    Field greenSpawn2 = new Field(366,3,true);
+    Field greenSpawn3 = new Field(330,39,true);
+    Field greenSpawn4 = new Field(366,39,true);
+
+    Field greenFinish1 = new Field(185,40,false);
+    Field greenFinish2 = new Field(185,76,false);
+    Field greenFinish3 = new Field(185,112,false);
+    Field greenFinish4 = new Field(185,148,false);
+
+    Field redSpawn1 = new Field(330,330,true);
+    Field redSpawn2 = new Field(366,330,true);
+    Field redSpawn3 = new Field(330,366,true);
+    Field redSpawn4 = new Field(367,366,true);
+
+    Field redFinish1 = new Field(329,184,false);
+    Field redFinish2 = new Field(293,184,false);
+    Field redFinish3 = new Field(257,184,false);
+    Field redFinish4 = new Field(221,184,false);
+
+//  FIELDS ON THE BOARD
+    Field blueStart = new Field(149,365,false);
+    Field fieldOfPath1 = new Field(149,330,false);
+    Field fieldOfPath2 = new Field(149,294,false);
+    Field fieldOfPath3 = new Field(149,258,false);
+    Field fieldOfPath4 = new Field(149,221,false);
+    Field fieldOfPath5 = new Field(112,221,false);
+    Field fieldOfPath6 = new Field(75,221,false);
+    Field fieldOfPath7 = new Field(40,221,false);
+    Field fieldOfPath8 = new Field(4,221,false);
+    Field fieldOfPath9 = new Field(4,184,false);
+    Field yellowStart = new Field(4,148,false);
+    Field fieldOfPath10 = new Field(40,148,false);
+    Field fieldOfPath11 = new Field(76,148,false);
+    Field fieldOfPath12 = new Field(112,148,false);
+    Field fieldOfPath13 = new Field(149,148,false);
+    Field fieldOfPath14 = new Field(149,112,false);
+    Field fieldOfPath15 = new Field(149,76,false);
+    Field fieldOfPath16 = new Field(149,40,false);
+    Field fieldOfPath17 = new Field(149,4,false);
+    Field fieldOfPath18 = new Field(185,4,false);
+    Field greenStart = new Field(221,4,false);
+    Field fieldOfPath19 = new Field(221,40,false);
+    Field fieldOfPath20 = new Field(221,76,false);
+    Field fieldOfPath21 = new Field(221,112,false);
+    Field fieldOfPath22 = new Field(221,148,false);
+    Field fieldOfPath23 = new Field(257,148,false);
+    Field fieldOfPath24 = new Field(293,148,false);
+    Field fieldOfPath25 = new Field(329,148,false);
+    Field fieldOfPath26 = new Field(365,148,false);
+    Field fieldOfPath27 = new Field(365,184,false);
+    Field redStart = new Field(365,221,false);
+    Field fieldOfPath28 = new Field(329,221,false);
+    Field fieldOfPath29 = new Field(293,221,false);
+    Field fieldOfPath30 = new Field(257,221,false);
+    Field fieldOfPath31 = new Field(221,221,false);
+    Field fieldOfPath32 = new Field(221,258,false);
+    Field fieldOfPath33 = new Field(221,293,false);
+    Field fieldOfPath34 = new Field(221,330,false);
+    Field fieldOfPath35 = new Field(221,365,false);
+    Field fieldOfPath36 = new Field(185,365,false);
+
+
+
+
     public void test(ActionEvent actionEvent) {
+
     }
 }
 
---
+++ src/sample/Field.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Field {
+    int positionX;
+    int positionY;
+    boolean taken;
+
+    public Field(int positionX, int positionY, boolean taken) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.taken = taken;
+    }
+}
