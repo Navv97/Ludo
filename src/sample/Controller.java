16f0c7bdfbe59538ede1d1f681af98197c944f23
Navv <grzesiek.kab@gmail.com>
2018-12-29 14:14:11 +0100

    board with pawns added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class Controller {
+
+    public void test(ActionEvent actionEvent) {
+    }
 }
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("board.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 450));
+        primaryStage.setMaxWidth(700);
+        primaryStage.setMaxHeight(450);
+        primaryStage.setMinWidth(700);
+        primaryStage.setMinHeight(450);
         primaryStage.show();
     }
 
